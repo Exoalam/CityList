0430e5c0984b1231d8a0f4463d05cc6c0aee1ebd
Exoalam <nafiul.arpon@gmail.com>
2022-06-14 21:38:24 +0600

    Ui-Test Done

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -75,5 +75,21 @@ public class MainActivityTest {
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
         Espresso.pressBack(); //Back button
     }
+    @Test
+    public void testShowActivity()
+    {
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+
+        onView(withId(R.id.showActivity)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_back)).check(matches(withText("Edmonton")));
+        onView(withId(R.id.button_back)).perform(click());
+         onView(withId(R.id.MainActivity)).check(matches(isDisplayed()));
+         Espresso.pressBack();
+
+    }
 
 }
